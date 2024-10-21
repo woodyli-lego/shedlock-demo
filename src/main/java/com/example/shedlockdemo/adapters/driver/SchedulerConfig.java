7293624e9e121452e02cff117c7eec9d07cb2ebf
Woody Li <woody.li@lego.com>
2024-10-22 07:51:07 +0800

    init

---
+++ src/main/java/com/example/shedlockdemo/adapters/driver/SchedulerConfig.java
@@ -0,0 +1,11 @@
+package com.example.shedlockdemo.adapters.driver;
+
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "1m")
+public class SchedulerConfig {
+}
